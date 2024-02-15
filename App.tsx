d9fdcfdb409dd8c3aa40a35f820c34f0ba46499a
S1D007 <s1d.official011001011@gmail.com>
2024-02-15 22:16:42 +0530

    some random push

--- App.tsx
+++ App.tsx
@@ -1,118 +1,61 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import {View, Text} from 'react-native';
+import React, {useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen';
+import CartMain from './src/screens/Cart/CartMain';
+import ProfileMain from './src/screens/Profile/ProfileMain';
+import Signup from './src/screens/Auth/Signup';
+import Main from './src/screens/Home/Main';
+import Login from './src/screens/Auth/Login';
+import OTP from './src/screens/Auth/OTP';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import Toast from 'react-native-toast-message'
+import { useAuth } from './src/zustand/useAuth';
+import ProductMain from './src/screens/Product/ProductMain';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const Stack = createStackNavigator();
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+const App = () => {
+  const {getToken, token, getUserData, user} = useAuth()
+  useEffect(() => {
+    getToken()
+    getUserData(token)
+    SplashScreen.hide();
+  }, [token]);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <GestureHandlerRootView style={{flex: 1}}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Main">
+          <Stack.Screen name="Main" options={{
+            headerShown: false,
+          }} component={Main} />
+          <Stack.Screen
+            options={{
+              headerShown: false,
+              animationTypeForReplace: 'pop',
+            }}
+            name="Login"
+            component={Login}
+          />
+          <Stack.Screen name="OTP" component={OTP} />
+          <Stack.Screen name="ProductMain" options={{
+            headerTitle: 'Bellissimo Toilet Cleaner Original Blue',
+            headerTitleStyle:{
+              fontFamily: 'Gilroy-Bold',
+              fontSize: 16,
+            },
+            headerShown: true,
+          }} component={ProductMain} />
+          <Stack.Screen options={{
+            headerShown: false,
+          }} name="Signup" component={Signup} />
+        </Stack.Navigator>
+      </NavigationContainer>
+      <Toast />
+    </GestureHandlerRootView>
   );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+};
 
 export default App;
